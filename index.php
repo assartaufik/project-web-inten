8b72bc6ae360cdea62d0ef7de7555276e49f31fd
assartaufik <assartaufik3@gmail.com>
2017-04-09 11:52:36 +0700

    ini cuma tampilan saja

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<title> Sistek akademik </title>
+<head>
+<body bgcolor="grey">
+<form 	method="post" name="akademik" action=".php">
+<table border=1 align="center" cellpadding=5 cellspacing=1 width="100%">
+<tr>
+	<th colspan="3" align="center" bgcolor="black" height="100"><font size="30"><font color="yellow">Sistem Akademik</font></th>
+</tr>
+<tr>
+	<td height="475" width="" bgcolor="" align="center">
+		<font size="25">Selamat Datang</font><br><br><br>
+		<font size="5"><a href="siswa/index.php">Lihat Data Siswa</a> || <a href="guru/index.php">Lihat Data Guru</a> || <a href="pegawai/index.php">Lihat data Pegawai</a>|| 
+		 || <br> <a href="mata pelajaran/index.php">Lihat Mata Pelajaran</a><font>
+	<td colspan="3" height="475" width="100" align="center">
+	<h2>PILIHAN MENU</h2>
+	<font size="4">
+	<a href="siswa/add.php"> Masukkan Data Siswa </a>
+	<br><br>
+	<a href="guru/add.php"> Masukkan Data Guru </a>
+	<br><br>
+	<a href="pegawai/add.php"> Masukkan Data Pegawai </a>
+	<br><br>
+	<a href="mata pelajaran/add.php">Masukan Mata Pelajaran</a>
+	
+	</font>
+	</td>
+<tr>
+	<td colspan="3" align="center" width="50">Copyright &#169 Assar Taufik H. </td>
+</tr>
+</table>
+</body>
+</html>
